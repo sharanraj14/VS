fc464710dcb2c88bc9776c3ef2bf8cb3e8dcd9d1
sharanraj14 <sharanraj140104@gmail.com>
2025-12-04 22:07:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   experimental: {
     appDir: true, // enables /app directory routing
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**', // allow loading images from any host
+        protocol: "https",
+        hostname: "**", // allow external images from any host
       },
     ],
   },
