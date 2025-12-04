7f306e0c89488f11194b14c2b950abc13cb53496
sharanraj14 <sharanraj140104@gmail.com>
2025-12-04 23:05:39 +0530

    Add files via upload

---
+++ src/app/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 p-4 text-white text-center">
+      &copy; 2025 Virtual Switch. All rights reserved.
+    </footer>
+  );
+}
---
+++ src/app/Header.js
@@ -0,0 +1,15 @@
+'use client';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="bg-black p-4 text-white">
+      <nav className="flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+}
