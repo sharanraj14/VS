554995b5b51c710a48758c23c85593f1076f51d8
sharanraj14 <sharanraj140104@gmail.com>
2025-12-04 23:09:24 +0530

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 p-4 text-white text-center">
+      &copy; 2025 Virtual Switch. All rights reserved.
+    </footer>
+  );
+}
 
